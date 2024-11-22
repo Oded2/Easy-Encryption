7351d2a48e711fd2206db2c80c0922ca2d240d7d
Oded Benamotz <obenamotz@gmail.com>
2024-11-22 17:29:56 +0200

    Added font awesome

---
+++ src/lib/components/Password.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import "@fortawesome/fontawesome-free/css/all.min.css";
+
+  let hidePass = true;
+  let password: string;
+</script>
+
+<div class="relative max-w-xs">
+  {#if hidePass}
+    <input
+      type="password"
+      class="input input-bordered !outline-none w-full pr-10"
+      placeholder="Enter your password"
+      bind:value={password}
+    />
+  {:else}
+    <input
+      type="text"
+      class="input input-bordered !outline-none w-full pr-10"
+      placeholder="Enter your password"
+      bind:value={password}
+    />
+  {/if}
+  <button
+    aria-label="Show Password"
+    type="button"
+    class="absolute end-2 top-3 text-gray-500"
+    on:click={() => (hidePass = !hidePass)}
+  >
+    {#if hidePass}
+      <i class="fa-solid fa-eye"></i>
+    {:else}
+      <i class="fa-solid fa-eye-slash"></i>
+    {/if}
+  </button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
+  import Password from "./../lib/components/Password.svelte";
   import Textbox from "$lib/components/Textbox.svelte";
   let encrypted: string;
 </script>
 
 <div class="container mx-auto px-2 sm:px-0 my-10 font-quicksand">
-  <div class="row-span-2 grid grid-cols-2 h-96">
+  <div class="md:grid grid-cols-2 lg:h-96">
     <div class="col-auto">
       <h1 class="font-bold text-5xl">Enter Text to Encrypt</h1>
       <Textbox bind:val={encrypted}></Textbox>
-      <div class="border-b-2"></div>
+      <div class="border-b-2 mb-5"></div>
+      <Password></Password>
     </div>
-
     <div class="col-auto">hello earth</div>
   </div>
 </div>
