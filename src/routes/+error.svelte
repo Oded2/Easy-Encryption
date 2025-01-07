9c190e6027ac343c80db4b45b6c95bf6042a038e
Oded Benamotz <obenamotz@gmail.com>
2025-01-07 18:45:26 +0200

    Error page now changes title after DOM load

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import { onMount } from "svelte";
+
   const url = $page;
+
+  onMount(() => (document.title = `${url.status} Error`));
 </script>
 
 <div class="flex h-screen w-full flex-col items-center justify-center gap-2">
@@ -8,5 +12,3 @@
   <h2 class="text-3xl">{url.error?.message}</h2>
   <a href="/" class="btn btn-neutral mt-2 rounded-3xl">Back to Home</a>
 </div>
-
-<svelte:head><title>{`${url.status} Error`}</title></svelte:head>
