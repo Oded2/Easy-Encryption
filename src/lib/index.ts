7034124775c3cace468d42bdfec1f7515ba16277
Oded Benamotz <obenamotz@gmail.com>
2025-01-20 20:56:51 +0200

    Minor change

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,11 +3,9 @@ import * as crypto from "crypto-ts";
 export function encrypt(text: string, password: string): string {
   return crypto.AES.encrypt(text, password).toString();
 }
-export function decrypt(encryptedText: string, password: string): string {
+export function decrypt(text: string, password: string): string {
   try {
-    return crypto.AES.decrypt(encryptedText, password).toString(
-      crypto.enc.Utf8
-    );
+    return crypto.AES.decrypt(text, password).toString(crypto.enc.Utf8);
   } catch (error) {
     console.debug(error);
   }
