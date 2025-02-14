e0f8cc96fe470637aea0968e7cb1b30026ee690c
Oded2 <obenamotz@gmail.com>
2025-02-15 01:28:55 +0200

    API uses POST instead of GET

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,21 +1,24 @@
 import { decrypt, encrypt } from "$lib";
-import { error, type RequestHandler } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async ({ url }) => {
-  const action = url.searchParams.get("action");
-  const text = url.searchParams.get("text");
-  const password = url.searchParams.get("password") ?? "";
-  if (!action) error(400, { message: "Action is required" });
-  if (!text) error(400, { message: "Text is required" });
+export async function POST({ request }) {
+  if (!request.body) throw error(400, { message: "Body cannot be empty" });
+  const data = await request.json();
+  const { action, text, password }: { [key: string]: string | undefined } =
+    data;
+  if (!action) throw error(400, { message: "Action is required" });
+  if (!text) throw error(400, { message: "Text is required" });
   if (!(action === "encrypt" || action === "decrypt"))
-    error(400, { message: "Invalid action" });
+    throw error(400, { message: "Invalid action" });
   const result: string =
-    action === "encrypt" ? encrypt(text, password) : decrypt(text, password);
-  if (result.length == 0) error(401, { message: "Invalid password" });
-  const data: { result: string } = {
+    action === "encrypt"
+      ? encrypt(text, password ?? "")
+      : decrypt(text, password ?? "");
+  if (result.length == 0) throw error(401, { message: "Invalid password" });
+  const resultData: { result: string } = {
     result,
   };
-  return new Response(JSON.stringify(data), {
+  return new Response(JSON.stringify(resultData), {
     headers: { "Content-Type": "application/json" },
   });
-};
+}
--- src/routes/api/shorten/+server.ts
+++ src/routes/api/shorten/+server.ts
@@ -5,7 +5,8 @@ export async function POST({ request }) {
   const apiUrl = "https://tinyurl.com/api-create.php";
   const text = await request.text();
   const response = await fetch(addParams(apiUrl, { url: text }));
-  if (!response.ok) error(response.status, { message: response.statusText });
+  if (!response.ok)
+    throw error(response.status, { message: response.statusText });
   const shortUrl = await response.text();
   return new Response(shortUrl, {
     headers: { "Content-Type": "text/plain" },
