c506abff90910a0db2e3ab4515a0e3f65fafb558
Oded2 <obenamotz@gmail.com>
2025-02-14 11:42:43 +0200

    Set theme to false

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,20 @@
-import type { Config } from 'tailwindcss';
-import daisyui from "daisyui"
-import defaultTheme from "tailwindcss/defaultTheme"
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
     extend: {
       fontFamily: {
-        quicksand: ['"Quicksand"', ...defaultTheme.fontFamily.sans]
-      }
-    }
+        quicksand: ['"Quicksand"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
 
   plugins: [daisyui],
   daisyui: {
-    themes: ["light"], // Add desired themes here
-    base: true, // Include base styles
-    styled: true, // Include component styles
-    utils: true, // Include utility classes
-    logs: true, // Show logs in the console
-    rtl: false, // Disable right-to-left support
-    darkTheme: "dark", // Default dark theme
+    themes: false,
   },
 } satisfies Config;
