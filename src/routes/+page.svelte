7afc6706f891f578c814b15e0e7a1bf60e4f8e4f
Oded2 <obenamotz@gmail.com>
2025-02-16 16:46:03 +0200

    Minor change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -266,7 +266,7 @@
 <button
   aria-label="Share"
   onclick={() => showModal("share")}
-  class=" btn btn-circle btn-primary hidden md:block fixed right-5 bottom-5"
+  class="btn btn-circle btn-primary hidden md:block fixed right-5 bottom-5"
 >
   <i class="fa-solid fa-share text-lg"></i>
 </button>
