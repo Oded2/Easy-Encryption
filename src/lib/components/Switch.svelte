5237ad9af4e518c37bd6d0fe5c1cd0b0305a20e6
Oded2 <obenamotz@gmail.com>
2025-02-17 00:14:55 +0200

    Optimized switch

--- src/lib/components/Switch.svelte
+++ src/lib/components/Switch.svelte
@@ -4,23 +4,20 @@
     state = $bindable(false),
     disclaimer = "",
   }: {
-    checked?: boolean;
     text?: string;
     state: boolean;
     disclaimer?: string;
   } = $props();
 </script>
 
-<div class="flex flex-col">
-  <label class="label cursor-pointer justify-center gap-2">
-    {#if disclaimer.length > 0}
-      <div class="tooltip cursor-default" data-tip={disclaimer}>
-        <i class="fa-solid fa-circle-exclamation"></i>
-      </div>
-    {/if}
-    {#if text.length > 0}
-      <span class="label-text">{text}</span>
-    {/if}
-    <input type="checkbox" class="toggle" bind:checked={state} />
-  </label>
+<div class="label justify-center gap-2">
+  {#if disclaimer.length > 0}
+    <div class="tooltip" data-tip={disclaimer}>
+      <i class="fa-solid fa-circle-exclamation"></i>
+    </div>
+  {/if}
+  {#if text.length > 0}
+    <span class="label-text">{text}</span>
+  {/if}
+  <input type="checkbox" class="toggle" bind:checked={state} />
 </div>
