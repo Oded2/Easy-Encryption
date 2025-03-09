c8458a0ac84a4a44276800b29f663e8dcceafec3
Oded2 <obenamotz@gmail.com>
2025-03-10 00:00:24 +0200

    Added copy button component

---
+++ src/lib/components/CopyButton.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import { addToast } from "$lib/toasts";
+  import type { Snippet } from "svelte";
+
+  const {
+    text,
+    className,
+    modifier,
+    children,
+  }: {
+    text: string;
+    className: string;
+    modifier?: {
+      func: (str: string) => Promise<string>;
+      text: string;
+      activate: boolean;
+    };
+    children: Snippet;
+  } = $props();
+
+  const originalMessage = "Copy to Clipboard";
+  const copyConfirmation = "Copied!";
+  const timeoutDuration = 1500;
+
+  let reactiveTip: string = $state(originalMessage);
+  let timeout: NodeJS.Timeout | null = null;
+
+  async function copy(): Promise<void> {
+    const original = reactiveTip;
+    // If already showing copyMessage, clear and reset the timeout
+    if (original === copyConfirmation) {
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(
+        () => (reactiveTip = originalMessage),
+        timeoutDuration
+      );
+      return;
+    }
+    let toWrite: string = text;
+    if (modifier && modifier.activate) {
+      reactiveTip = modifier.text;
+      toWrite = await modifier.func(text);
+    }
+    try {
+      await navigator.clipboard.writeText(toWrite);
+      if (timeout) clearTimeout(timeout);
+      reactiveTip = copyConfirmation;
+    } catch (e) {
+      console.error(e);
+      addToast({
+        text: "Error Copying to Clipboard",
+        duration: 5000,
+        type: "error",
+      });
+    }
+    timeout = setTimeout(
+      () => (reactiveTip = originalMessage),
+      timeoutDuration
+    );
+  }
+</script>
+
+<div class="tooltip w-full" data-tip={reactiveTip}>
+  <button class={className} onclick={copy}>
+    {@render children()}
+  </button>
+</div>
--- src/lib/components/ShareButton.svelte
+++ src/lib/components/ShareButton.svelte
@@ -2,6 +2,7 @@
   import { closeModal, showModal } from "$lib";
   import { addToast } from "$lib/toasts";
   import { toDataURL } from "qrcode";
+  import CopyButton from "./CopyButton.svelte";
 
   const {
     title,
@@ -13,42 +14,8 @@
     shorten: boolean;
   } = $props();
 
-  const disabledMessage = "URL is too long";
-  const clipboardTip = "Copy to Clipboard";
-  const timeoutDuration = 1500;
-  const copyMessage = "Copied!";
   const disabled: boolean = $derived(link.length > 10000);
-  let reactiveTip: string = $state(clipboardTip);
   let inProgress: boolean = $state(false);
-  let timeout: NodeJS.Timeout | undefined;
-
-  async function copy(): Promise<void> {
-    const original = reactiveTip;
-    // If already showing copyMessage, clear and reset the timeout
-    if (original === copyMessage) {
-      if (timeout) clearTimeout(timeout);
-      timeout = setTimeout(() => (reactiveTip = clipboardTip), timeoutDuration);
-      return;
-    }
-    let toWrite: string = link;
-    if (shorten) {
-      reactiveTip = "Awaiting URL...";
-      toWrite = await shortenURL();
-    }
-    try {
-      await navigator.clipboard.writeText(toWrite);
-      if (timeout) clearTimeout(timeout);
-      reactiveTip = copyMessage;
-    } catch (e) {
-      console.error(e);
-      addToast({
-        text: "Error Copying to Clipboard",
-        duration: 5000,
-        type: "error",
-      });
-    }
-    timeout = setTimeout(() => (reactiveTip = clipboardTip), timeoutDuration);
-  }
 
   async function qr(): Promise<void> {
     const qrTitle = document.getElementById("qrTitle") as HTMLHeadingElement;
@@ -56,7 +23,7 @@
     const download = document.getElementById("qrDownload") as HTMLAnchorElement;
     let qrLink: string = link;
     inProgress = true;
-    if (link.length > 1000) qrLink = await shortenURL();
+    if (link.length > 1000) qrLink = await shortenURL(link);
     try {
       const url = await toDataURL(qrLink, { width: 400 });
       download.href = url;
@@ -77,34 +44,35 @@
     qrTitle.textContent = qrLink;
   }
 
-  async function shortenURL(): Promise<string> {
+  async function shortenURL(url: string): Promise<string> {
     const response = await fetch("/api/shorten", {
       method: "POST",
       headers: {
         "Content-Type": "text/plain",
       },
-      body: link,
+      body: url,
     });
     if (!response.ok) return link;
     return await response.text();
   }
 </script>
 
-<div class="w-full sm:w-3/4 mx-auto join" data-tip={reactiveTip}>
-  <div
-    class="tooltip w-full"
-    data-tip={disabled ? disabledMessage : reactiveTip}
+<div class="w-full sm:w-3/4 mx-auto join">
+  <CopyButton
+    text={link}
+    className="btn btn-neutral join-item w-full btn-outline"
+    modifier={{
+      func: shortenURL,
+      text: "Awaiting URL...",
+      activate: shorten,
+    }}
   >
-    <button
-      class="btn btn-neutral join-item w-full"
-      class:btn-outline={!disabled}
-      onclick={copy}
-      {disabled}
-    >
-      {title}
-    </button>
-  </div>
-  <div class="tooltip" data-tip={disabled ? disabledMessage : "Create QR Code"}>
+    {title}
+  </CopyButton>
+  <div
+    class="tooltip"
+    data-tip={disabled ? "URL is too long" : "Create QR Code"}
+  >
     <button
       class="btn btn-neutral btn-square join-item"
       class:skeleton={inProgress}
